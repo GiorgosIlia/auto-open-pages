fe086569faf17509c75f3a41d1932121527ef483
giorgos ilia <giorkos313@gmail.com>
2021-08-09 13:22:42 +0300

    Update openup.py

--- openup.py
+++ openup.py
@@ -1,7 +1,6 @@
 import pyautogui,time ,webbrowser
 sites = ['youtube.com','facebook.com' ]
 time.sleep(1)
-#pyautogui.click(483,1062)
 
 webbrowser.open('http://www.google.com')
 time.sleep(1)
@@ -13,4 +12,4 @@ for i in range (0, len(sites)) :
     pyautogui.press('enter')
     pyautogui.hotkey('ctrl', 't' )
     
-pyautogui.typewrite(' ')
+pyautogui.typewrite('enter','your','websites','here')
